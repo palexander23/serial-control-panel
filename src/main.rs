f319035afe6e468f8c6f05458d0d8c9da2594c26
pete <peter.d.alexander23@gmail.com>
2023-01-09 23:05:39 +0000

    Add Iced gui library and Counter example app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{
+    widget::{button, column, text},
+    Alignment, Element, Error, Sandbox, Settings,
+};
+
+struct Counter {
+    // The counter value
+    value: i32,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Message {
+    IncrementPressed,
+    DecrementPressed,
+}
+
+impl Sandbox for Counter {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self { value: 0 }
+    }
+
+    fn title(&self) -> String {
+        String::from("Counter Demo")
+    }
+
+    fn view(&self) -> Element<Message> {
+        // We use a column: a simple vertical layout
+        column![
+            // The increment button. We tell it to produce an
+            // `IncrementPressed` message when pressed
+            button("+").on_press(Message::IncrementPressed),
+            // We show the value of the counter here
+            text(self.value).size(50),
+            // The decrement button. We tell it to produce a
+            // `DecrementPressed` message when pressed
+            button("-").on_press(Message::DecrementPressed),
+        ]
+        .padding(20)
+        .align_items(Alignment::Center)
+        .into()
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::IncrementPressed => {
+                self.value += 1;
+            }
+            Message::DecrementPressed => {
+                self.value -= 1;
+            }
+        }
+    }
+}
+
+fn main() -> Result<(), Error> {
+    Counter::run(Settings::default())
 }
